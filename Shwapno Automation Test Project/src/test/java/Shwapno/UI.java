e6e9804a3f581fcd3c5ef20dba2fafa5efb12d2e
Shamima4407 <shamima4407@gmail.com>
2021-03-10 00:38:02 +0600

    First commit

--- Shwapno Automation Test Project/src/test/java/Shwapno/UI.java
+++ Shwapno Automation Test Project/src/test/java/Shwapno/UI.java
@@ -1,5 +1,7 @@
 package Shwapno;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -16,13 +18,14 @@ public class UI {
 		//Coding for chrome browser opening
 	    System.setProperty("webdriver.chrome.driver","C:\\Users\\HP-NPC\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe");
 	    driver = new ChromeDriver();
+	    driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
 	    driver.manage().window().maximize();
 	    driver.get("https://www.shwapno.com/");
 	    Thread.sleep(1000);
 	    //Code for select the city 
 	    Select city = new Select(driver.findElement(By.xpath("//*[@id=\"state\"]")));
 	    city.selectByVisibleText("Dhaka");
-	    //Code for select the city 
+	    //Code for select the area 
 	    Select area = new Select(driver.findElement(By.xpath("//*[@id=\"city\"]")));
 	    area.selectByVisibleText("Lalbagh");
 	    //Code for click the submit button
@@ -30,9 +33,9 @@ public class UI {
 	    Thread.sleep(5000);
 	    
 	    //Code for adding product in the cart
-	    driver.findElement(By.xpath("//*[@id=\"Productshowcase\"]/div[1]/div/div[3]/div/div[1]/a/img")).click();
+	    driver.findElement(By.xpath("//div[@class='bucket_left']/a/img")).click();
 	    driver.findElement(By.xpath("//*[@id=\"QuickAdd16777459\"]/a[3]")).click();
-	    Thread.sleep(8000);
+	    Thread.sleep(5000);
 	    driver.close();
 	}
 }
